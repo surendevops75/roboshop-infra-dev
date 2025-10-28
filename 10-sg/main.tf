e7f8b3e7ccb6ed5907681cc7c5e9b7512dca887c
surendevops75 <surendevops75@gmail.com>
2025-10-28 20:21:26 +0530

    terraform

--- 10-sg/main.tf
+++ 10-sg/main.tf
@@ -19,3 +19,14 @@ module "sg" {
   sg_description = "Created for ${var.sg_names[count.index]}"
   vpc_id = local.vpc_id
 }
+
+#Frontend accepting traffic from frontend ALB
+
+# resource "aws_security_group_rule" "frontend_frontend_alb" {
+#   type              = "ingress"
+#   security_group_id = module.sg[9].sg_id # frontend SG ID
+#   source_security_group_id = module.sg[11].sg_id # frontend ALB SG ID
+#   from_port         = 80
+#   protocol          = "tcp"
+#   to_port           = 80
+# }
