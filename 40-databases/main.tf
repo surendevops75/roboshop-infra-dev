b186a87a25aada78d230b3c959f979df24efc166
surendevops75 <surendevops75@gmail.com>
2025-10-29 14:19:06 +0530

    terraform

--- 40-databases/main.tf
+++ 40-databases/main.tf
@@ -78,3 +78,43 @@ resource "terraform_data" "redis" {
      ]
   }
 }
+
+resource "aws_instance" "rabbitmq" {
+  ami           = local.ami_id
+  instance_type = "t3.micro"
+  vpc_security_group_ids = [local.rabbitmq_sg_id]
+  subnet_id = local.database_subnet_id
+
+  tags = merge(
+    local.common_tags,
+    {
+        Name = "${local.common_name_suffix}-rabbitmq" #roboshop-dev-rabbitmq
+    }
+  )
+}
+
+resource "terraform_data" "rabbitmq" {
+  triggers_replace = [
+    aws_instance.rabbitmq.id
+  ]
+
+  connection {
+    type = "ssh"
+    user = "ec2-user"
+    password = "DevOps321"
+    host = aws_instance.rabbitmq.private_ip
+  }
+
+  # terraform copies this file to rabbitmq server
+  provisioner "file" {
+    source = "bootstrap.sh"
+    destination = "/tmp/bootstrap.sh"
+  }
+
+  provisioner "remote-exec" {
+    inline = [ 
+        "chmod +x /tmp/bootstrap.sh",
+        "sudo sh +x /tmp/bootstrap.sh redis"
+     ]
+  }
+}
