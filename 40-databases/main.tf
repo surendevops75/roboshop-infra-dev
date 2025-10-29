65381bc84318f986b95ec0fb6e3cc9fe1410bdfe
surendevops75 <surendevops75@gmail.com>
2025-10-29 12:26:15 +0530

    terraform

--- 40-databases/main.tf
+++ 40-databases/main.tf
@@ -1,4 +1,4 @@
-resource "aws_instance" "bastion" {
+resource "aws_instance" "mongodb" {
   ami           = local.ami_id
   instance_type = "t3.micro"
   vpc_security_group_ids = [local.mongodb_sg_id]
