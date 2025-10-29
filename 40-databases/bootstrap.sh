3a3cca8d4707764f620bb00739bfa9349679d0e3
surendevops75 <surendevops75@gmail.com>
2025-10-29 14:00:57 +0530

    terraform

--- 40-databases/bootstrap.sh
+++ 40-databases/bootstrap.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
+component=$1
 dnf install ansible -y
-ansible-pull -U https://github.com/surendevops75/ansible-roboshop-roles-tf.git -e component=mongodb main.yaml
+ansible-pull -U https://github.com/surendevops75/ansible-roboshop-roles-tf.git -e component=$component main.yaml
--- 40-databases/main.tf
+++ 40-databases/main.tf
@@ -33,7 +33,48 @@ resource "terraform_data" "mongodb" {
   provisioner "remote-exec" {
     inline = [ 
         "chmod +x /tmp/bootstrap.sh",
-        "sudo sh +x /tmp/bootstrap.sh"
+        # "sudo sh +x /tmp/bootstrap.sh"
+        "sudo sh +x /tmp/bootstrap.sh mongodb"
+     ]
+  }
+}
+
+resource "aws_instance" "redis" {
+  ami           = local.ami_id
+  instance_type = "t3.micro"
+  vpc_security_group_ids = [local.redis_sg_id]
+  subnet_id = local.database_subnet_id
+
+  tags = merge(
+    local.common_tags,
+    {
+        Name = "${local.common_name_suffix}-redis" #roboshop-dev-redis
+    }
+  )
+}
+
+resource "terraform_data" "redis" {
+  triggers_replace = [
+    aws_instance.redis.id
+  ]
+
+  connection {
+    type = "ssh"
+    user = "ec2-user"
+    password = "DevOps321"
+    host = aws_instance.redis.private_ip
+  }
+
+  # terraform copies this file to redis server
+  provisioner "file" {
+    source = "bootstrap.sh"
+    destination = "/tmp/bootstrap.sh"
+  }
+
+  provisioner "remote-exec" {
+    inline = [ 
+        "chmod +x /tmp/bootstrap.sh",
+        "sudo sh +x /tmp/bootstrap.sh redis"
      ]
   }
 }
