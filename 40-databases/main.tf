ce729a228ee64eddd33cdbccceb07082fe02cf40
surendevops75 <surendevops75@gmail.com>
2025-10-29 13:28:46 +0530

    terraform

--- 40-databases/main.tf
+++ 40-databases/main.tf
@@ -32,8 +32,8 @@ resource "terraform_data" "mongodb" {
 
   provisioner "remote-exec" {
     inline = [ 
-        "chmod +x /tmp/bootstrap.sh",
-        "sudo sh chmod +x /tmp/bootstrap.sh"
+        "chmod +x /tmp/bootstrap.sh"
+        "sudo sh +x /tmp/bootstrap.sh"
      ]
   }
 }
