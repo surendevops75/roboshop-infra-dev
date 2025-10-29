f3a2385e853b47fc6e4b8946db11430a9c1a331f
surendevops75 <surendevops75@gmail.com>
2025-10-29 12:11:53 +0530

    terraform

--- 30-sg-rules/data.tf
+++ 30-sg-rules/data.tf
@@ -5,3 +5,7 @@ data "aws_ssm_parameter" "backend_alb_sg_id" {
 data "aws_ssm_parameter" "bastion_sg_id" {
   name = "/${var.project_name}/${var.environment}/bastion_sg_id"
 }
+
+data "aws_ssm_parameter" "mongodb_sg_id" {
+  name = "/${var.project_name}/${var.environment}/mongodb_sg_id"
+}
