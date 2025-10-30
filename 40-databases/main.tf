5fce966fdd999b95ad700bc7af2db27567373fbd
surendevops75 <surendevops75@gmail.com>
2025-10-30 19:23:12 +0530

    terraform

--- 40-databases/main.tf
+++ 40-databases/main.tf
@@ -174,6 +174,7 @@ resource "aws_route53_record" "mongodb" {
   type    = "A"
   ttl     = 1
   records = [aws_instance.mongodb.private_ip]
+  allow_overwrite = true
 }
 
 resource "aws_route53_record" "redis" {
@@ -182,6 +183,7 @@ resource "aws_route53_record" "redis" {
   type    = "A"
   ttl     = 1
   records = [aws_instance.redis.private_ip]
+  allow_overwrite = true
 }
 
 resource "aws_route53_record" "mysql" {
@@ -190,6 +192,7 @@ resource "aws_route53_record" "mysql" {
   type    = "A"
   ttl     = 1
   records = [aws_instance.mysql.private_ip]
+  allow_overwrite = true
 }
 
 resource "aws_route53_record" "rabbitmq" {
@@ -198,6 +201,7 @@ resource "aws_route53_record" "rabbitmq" {
   type    = "A"
   ttl     = 1
   records = [aws_instance.rabbitmq.private_ip]
+  allow_overwrite = true
 }
 
 
