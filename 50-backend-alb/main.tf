7a231e11dd304f82f4e628dd11b8e2891fa10af6
surendevops75 <surendevops75@gmail.com>
2025-11-09 11:40:31 +0530

    terraform roboshop

--- 50-backend-alb/main.tf
+++ 50-backend-alb/main.tf
@@ -32,3 +32,17 @@ resource "aws_lb_listener" "front_end" {
     }
   }
 }
+resource "aws_route53_record" "backend_alb" {
+  zone_id = var.zone_id
+  name    = "*.backend_alb-${var.environment}.${var.domain_name}" 
+  type = "A"
+
+  alias {
+    # These are ALB details, not our domain details
+    name                   = aws_lb.backend_alb.dns_name
+    zone_id                = aws_lb.backend_alb.zone_id
+    evaluate_target_health = true
+  }
+}
+
+
--- 50-backend-alb/variables.tf
+++ 50-backend-alb/variables.tf
@@ -5,3 +5,11 @@ variable "project_name" {
 variable "environment" {
   default = "dev"
 }
+
+variable "zone_id" {
+  default = "Z10042263JCJ5ZTK2AT9J"
+}
+
+variable "domain_name" {
+  default = "surendevops.fun"
+}
