bd826f6989d7da35eb793058c01f58f0edce7712
surendevops75 <surendevops75@gmail.com>
2025-11-09 13:28:01 +0530

    terraform roboshop

--- 30-sg-rules/main.tf
+++ 30-sg-rules/main.tf
@@ -74,7 +74,7 @@ resource "aws_security_group_rule" "catalogue_backend_alb" {
   type              = "ingress"
   security_group_id = local.catalogue_sg_id
   source_security_group_id = local.backend_alb_sg_id
-  from_port         = 22
+  from_port         = 8080
   protocol          = "tcp"
-  to_port           = 27017
+  to_port           = 8080
 }
