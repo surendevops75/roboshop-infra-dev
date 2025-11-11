8407d4f4125fad62d671e7aba7592842d69caef0
surendevops75 <surendevops75@gmail.com>
2025-11-11 17:14:31 +0530

    terraform roboshop

--- 60-catalogue/main.tf
+++ 60-catalogue/main.tf
@@ -86,6 +86,9 @@ resource "aws_launch_template" "catalogue" {
   instance_type = "t3.micro"
   vpc_security_group_ids = [local.catalogue_sg_id]
 
+  #when we run terraform apply again, a new version will be created with new AMI
+  update_default_version = true
+
   # tags attached to the instance
   tag_specifications {
     resource_type = "instance"
@@ -134,6 +137,14 @@ resource "aws_autoscaling_group" "catalogue" {
   }
   vpc_zone_identifier       = local.private_subnet_ids
   target_group_arns = [aws_lb_target_group.catalogue.arn]
+
+  instance_refresh {
+    strategy = "Rolling"
+    preferences {
+      min_healthy_percentage = 50 # atleast 50% of the instances should be up and running
+    }
+    triggers = ["launch_template"]
+  }
   
   dynamic "tag" {  # we will get the iterator with name as tag
     for_each = merge(
